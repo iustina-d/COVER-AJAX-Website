5f535ed5e77461ab67f0df3260fd206f7404d19c
iustina-d <iustina2dulea@gmail.com>
2023-01-25 01:05:41 +0200

    creating the product route

--- server.py
+++ server.py
@@ -19,6 +19,10 @@ def products():
   all_products = data_manager.get_all_products()
   return render_template("products.html",products=all_products)
 
+@app.route("/product/<int:id>")
+def product(id):
+  get_product = data_manager.get_product(id)
+  return render_template("product.html",product=get_product)
 
 if __name__ == "__main__":
     app.run(debug=True)
