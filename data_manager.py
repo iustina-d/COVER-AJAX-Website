0c461f255cfcac4f33d93bbd5c176dc32d56b741
iustina-d <iustina2dulea@gmail.com>
2023-01-25 01:06:36 +0200

    extract all products from DB

--- data_manager.py
+++ data_manager.py
@@ -4,8 +4,14 @@ import database_common
 
 @database_common.connection_handler
 def get_all_products(cursor):
-    cursor.execute("SELECT * FROM products ORDER BY product_name LIMIT 20;")
+    cursor.execute("SELECT * FROM products ORDER BY product_name;")
     return cursor.fetchall()
+
+@database_common.connection_handler
+def get_product(cursor,id):
+    cursor.execute("SELECT * FROM products WHERE id = %(id)s ;",
+    {"id":id})
+    return cursor.fetchone()
 
 
 
