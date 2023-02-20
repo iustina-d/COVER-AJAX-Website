1bdc1bfd49bad092eecd0b88af9fcd0e218ee20e
iustina-d <iustina2dulea@gmail.com>
2023-02-20 21:54:06 +0200

    updates

---
+++ nix/store/01wmpmcl64yija6f8darlwzbrnmgryrb-python3.8-docutils-0.16/bin/rst2odt_prepstyles.py
@@ -0,0 +1,4 @@
+#! /nix/store/2jysm3dfsgby5sw5jgj43qjrb5v79ms9-bash-4.4-p23/bin/bash -e
+export PATH='/nix/store/z65l1jqvxa58zzwwa3bvglb6asj4y8cv-python3-3.8.5/bin:/nix/store/01wmpmcl64yija6f8darlwzbrnmgryrb-python3.8-docutils-0.16/bin'${PATH:+':'}$PATH
+export PYTHONNOUSERSITE='true'
+exec -a "$0" "/nix/store/01wmpmcl64yija6f8darlwzbrnmgryrb-python3.8-docutils-0.16/bin/.rst2odt_prepstyles.py-wrapped"  "$@"
